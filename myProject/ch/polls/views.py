a25a6450a878b22734692fa94248d3be32e54141
kkh256 <kyudang2@gmail.com>
2018-09-28 16:01:27 +0900

    Final chapter5 - add log, chnage function-base view -> class view

--- myProject/ch/polls/views.py
+++ myProject/ch/polls/views.py
@@ -1,26 +1,54 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-
+from django.views import generic
 from polls.models import Choice, Question
 
-# Create your views here.
+import logging
+logger = logging.getLogger(__name__)
 
 
+#-- Class-based GenericView
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        '''최근에 생성된 질문 5개를 리턴'''
+        return Question.objects.order_by('-pub_date')[:5]
+
+'''  function-based view
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+'''
 
+#-- Class-based GenericView
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+'''  function-based view
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request,'polls/detail.html', {'question': question})
+'''
 
+#-- Class-based GenericView
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+''' function-based view
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+'''
 
+#  function-based view
 def vote(request, question_id):
+    logger.debug("vote().question_id: %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -34,5 +62,3 @@ def vote(request, question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
